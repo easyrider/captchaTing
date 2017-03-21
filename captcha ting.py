11350442f05efd8ed844f1022aac679fc6b98123
ryan9918 <ryan9918@users.noreply.github.com>
2017-03-21 20:40:00 +0000

    Add files via upload

--- captcha ting.py
+++ captcha ting.py
@@ -1,13 +1,16 @@
 # captchaTing.py
 
-
-from functions import storeTokens
-
 from datetime import datetime
 import webbrowser
 from flask import Flask, render_template, request
 import _thread
 import logging
+
+tokens = []
+
+def stamp():
+    timestamp = str("["+datetime.utcnow().strftime("%H:%M:%S")+"]")
+    return timestamp
 
 app = Flask(__name__)
 
@@ -17,25 +20,25 @@ log.setLevel(logging.ERROR)
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/solve', methods=['GET', 'POST'])
 
-tokens = {}
-
 def solve():
-    sitekey = "6Ld80SYTAAAAABkkLnWCwycH-8K9axi95wjeG7lb" # SITEKEY GOES HERE FAM
+    sitekey = "6LeWwRkUAAAAAOBsau7KpuC9AV-6J8mhw4AjC3Xz" # SITEKEY GOES HERE FAM
     if request.method == "POST":
             token = request.form.get('g-recaptcha-response', '')
             tokens.append(token)
-            print("stored token using {}".format(sitekey))
+            count = len(tokens)
+            print("{} stored token | {} total stored".format(stamp(), count))
     return render_template('index.html', sitekey=sitekey)
 
 def harvestTokens():
     _thread.start_new_thread(app.run, ())
-    webbrowser.open("http://supremenewyork:5000/solve") # WEB ADDRESS FOR SOLVING CAPTCHA GOES HERE FAM
+    webbrowser.open("http://supremenewyork.com:5000/solve") # WEB ADDRESS FOR SOLVING CAPTCHA GOES HERE FAM
     return
 
 def main():
 	print("captcha ting we out here")
 	print("this shit was made by the real chef, cos he the real chef\n\n")
 	harvestTokens()
+	input("")
 
 if __name__ == '__main__':
 	main()
